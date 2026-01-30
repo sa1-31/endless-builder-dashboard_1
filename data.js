86ef017ae354f57792ef208d53a5824cf5a61f65
Sa1 <abde88227@gmail.com>
2026-01-31 03:58:52 +0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const builders = [
+  {
+    name: "Builder Alpha",
+    contributions: 14,
+    level: "Gold",
+    types: ["Code", "Infra"]
+  },
+  {
+    name: "Builder Beta",
+    contributions: 11,
+    level: "Silver",
+    types: ["Frontend", "Docs"]
+  },
+  {
+    name: "Builder Gamma",
+    contributions: 8,
+    level: "Bronze",
+    types: ["Tools", "SDK"]
+  }
+];
